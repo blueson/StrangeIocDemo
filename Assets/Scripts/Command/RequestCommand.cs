1788a6c3904281abc551681980b9d382abc1ef03
blueson <778252084@qq.com>
2018-10-14 21:14:31 +0800

    service dispatcher command

--- Assets/Scripts/Command/RequestCommand.cs
+++ Assets/Scripts/Command/RequestCommand.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using strange.extensions.command.impl;
+using strange.extensions.dispatcher.eventdispatcher.api;
 using UnityEngine;
 
-public class RequestCommand : Command {
-
+public class RequestCommand : Command
+{
     [Inject]
     public IScoresService scoreService { get; set; }
 
     public override void Execute()
     {
+        Retain();
+        scoreService.dispatcher.AddListener(ServiceEvent.RequestScores, this.OnComplete);
         scoreService.RequestScores("http://www.baidu.com");
+    }
+
+    public void OnComplete(IEvent evt)
+    {
+        Debug.Log("OnComplete" + evt.data);
+        scoreService.dispatcher.RemoveListener(ServiceEvent.RequestScores, this.OnComplete);
+
+        Release();
     }
 }
--- Assets/Scripts/Service/ScoreService.cs
+++ Assets/Scripts/Service/ScoreService.cs
@@ -7,11 +7,13 @@ public class ScoreService : IScoresService
     public void OnCallback()
     {
         int score = Random.Range(0, 100);
+        dispatcher.Dispatch(ServiceEvent.RequestScores, score);
     }
 
     public void RequestScores(string url)
     {
         Debug.Log(url);
+        OnCallback();
     }
 
     public void UpdateScores(string url, int score)
@@ -19,5 +21,6 @@ public class ScoreService : IScoresService
         Debug.Log(url + "   " + score);
     }
 
+    [Inject]
     public strange.extensions.dispatcher.eventdispatcher.api.IEventDispatcher dispatcher { get; set; }
 }
